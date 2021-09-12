503b7687bb612e6ac53895e633ddf1af134f0188
JANGHYUN KIM <janghyun0402@gmail.com>
2021-09-12 12:14:57 +0900

    Information check, modify using UDP

--- src/UDP_Demo/Client.java
+++ src/UDP_Demo/Client.java
@@ -2,38 +2,93 @@ package UDP_Demo;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Client {
+    public static int byteToShort(byte[] bytes) {
+
+        int newValue = 0;
+        newValue |= (((int) bytes[0]) << 8) & 0xFF00;
+        newValue |= (((int) bytes[1])) & 0xFF;
+
+        return newValue;
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket ds = new DatagramSocket();
+        DatagramSocket dsSend = new DatagramSocket();
+        DatagramSocket dsReceive = new DatagramSocket(7000);
+
+        InetAddress ia = InetAddress.getByName("127.0.0.1");
 
         System.out.println("Select Mode(1. Check    2. Modify)");
         int mode;
         Scanner sc = new Scanner(System.in);
         mode = sc.nextInt();
+        sc.nextLine();              //앞에서 생긴 개행문자 버려서 뒤의 nextline 스킵되는거 방지.
+
+        byte[] bufferToSend = new byte[1000];           //보낼 정보 저장하는 버퍼
+        bufferToSend[0] = 'A';
+        bufferToSend[1] = 'T';
+        bufferToSend[2] = 'L';
+        bufferToSend[3] = 'N';
+
 
         if(mode == 1){              //설정값 조회 - 요청 보내고, 답변 받고, 출력
+            byte[] bufferToReceive = new byte[1000];        //값 받아올 버퍼
+
+            short bufferlen = 7;
+            bufferToSend[6] = (byte)0x0;      //STX, ID, length 크기 제외하고 7부터 payload 시작
+
+            bufferlen -= 6;
+            bufferToSend[4] = (byte) (bufferlen & 0xFF);    //4, 5 index에 length 정보 세팅
+            bufferToSend[5] = (byte) ((bufferlen >> 8 )& 0xFF);
+
+            System.out.println(bufferToSend[4] + " " + bufferToSend[5]);
+
+
+            DatagramPacket dpSend = new DatagramPacket(bufferToSend, bufferToSend.length, ia, 6999);
+            dsSend.send(dpSend);
+
+
+            DatagramPacket dpReceive = new DatagramPacket(bufferToReceive, bufferToReceive.length);
+            dsReceive.receive(dpReceive);
+
+            String rivoInfo = new String(bufferToReceive, 7, byteToShort(new byte[]{bufferToReceive[5], bufferToReceive[4]}));
+            System.out.println("rivoInfo : " + rivoInfo);
 
         }
         else if(mode == 2){         //설정값 변경 - 요청 보내고, 변경된 값으로 답변 받고, 출력
+            byte[] bufferToReceive = new byte[1000];        //값 받아올 버퍼
+            short bufferlen;
+            String newRivoInfo;
+            System.out.print("Enter new rivoInfo: ");
+            newRivoInfo = sc.nextLine();                         //수정할 값 입력받음
+
+            bufferlen = (short) newRivoInfo.length();
+            bufferToSend[6] = (byte)0x1;      //STX, ID, length 크기 제외하고 7부터 payload 시작
+
+            bufferToSend[4] = (byte) (bufferlen & 0xFF);    //4, 5 index에 length 정보 세팅 (short to byte array)
+            bufferToSend[5] = (byte) ((bufferlen >> 8 )& 0xFF);
+
+            System.out.println("bufferlen: " + bufferlen);
+
+            System.arraycopy(newRivoInfo.getBytes(), 0, bufferToSend, 7, bufferlen);
+
+            DatagramPacket dpSend = new DatagramPacket(bufferToSend, bufferToSend.length, ia, 6999);
+            dsSend.send(dpSend);            //요청 패킷 전송
+
+            DatagramPacket dpReceive = new DatagramPacket(bufferToReceive, bufferToReceive.length);
+            dsReceive.receive(dpReceive);
+
+            //변경된 설정값 Server로부터 다시 받아서 저장
+            System.out.println(bufferToReceive[4] + " " + bufferToReceive[5]);        //[4] = 5, [5] = 0
+            String rivoInfo = new String(bufferToReceive, 7, byteToShort(new byte[]{bufferToReceive[5], bufferToReceive[4]}));
+            System.out.println("rivoInfo has been successfully changed to [" + rivoInfo + "]");
 
         }
 
-
-
-
-        /*
-        System.out.print("Input to send: ");
-        String msg = in.readLine();
-        InetAddress ia = InetAddress.getByName("127.0.0.1");
-
-        DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, ia, 6999);
-        ds.send(dp);
-         */
-
-        System.out.println("Successfully Sended");
     }
 }
 
--- src/UDP_Demo/Server.java
+++ src/UDP_Demo/Server.java
@@ -2,17 +2,71 @@ package UDP_Demo;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
+    public static int byteToShort(byte[] bytes) {           //byte arr에서 short 값 추출
+
+        int newValue = 0;
+        newValue |= (((int) bytes[0]) << 8) & 0xFF00;
+        newValue |= (((int) bytes[1])) & 0xFF;
+
+        return newValue;
+    }
+
     public static void main(String[] args) throws IOException{
-        String setting = new String("Original");
-        DatagramSocket ds = new DatagramSocket(6999);
-        byte[] buffer = new byte[1000];
+        String rivoInfo = "Hello";
+
+        DatagramSocket dsSend = new DatagramSocket();
+        DatagramSocket dsReceive = new DatagramSocket(6999);
+        InetAddress ia = InetAddress.getByName("127.0.0.1");
+
+        byte[] bufferToReceive = new byte[1000];
+        DatagramPacket dpReceive = new DatagramPacket(bufferToReceive, bufferToReceive.length);        //Client가 보낸 요청 수신
+        dsReceive.receive(dpReceive);
 
 
+        if(bufferToReceive[6] == 0x0){              //설정값 조회 == GET
+            byte[] bufferToSend = new byte[1000];
+            short bufferlen = (short) rivoInfo.length();
 
-        DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
-        ds.receive(dp);
-        System.out.println(dp.getAddress().getHostAddress() + " > " + new String(dp.getData()).trim());
+            bufferToSend[4] = (byte) (bufferlen & 0xFF);    //4, 5 index에 length 정보 세팅
+            bufferToSend[5] = (byte) ((bufferlen >> 8 )& 0xFF);
+            bufferToSend[6] = 0x0;
+
+            //System.out.println("before arraycopy");
+
+            //bufferToSend[7] 부터 rivoInfo 값 저장 -> arraycopy 함수 이용 (string to byte array)
+            System.arraycopy(rivoInfo.getBytes(), 0, bufferToSend, 7, bufferlen);
+
+            DatagramPacket dpSend = new DatagramPacket(bufferToSend, bufferToSend.length, ia, 7000);
+            dsSend.send(dpSend);
+            System.out.println("Send rivoInfo [" + rivoInfo + "]");
+        }
+        else if(bufferToReceive[6] == 0x1){         //설정값 변경 == SET
+            byte[] bufferToSend = new byte[1000];
+            short bufferlen;
+
+            bufferlen = (short) byteToShort(new byte[]{bufferToReceive[5], bufferToReceive[4]});
+            System.out.println("bufferlen: " + bufferlen);
+
+            //Client에서 전달받은 newRivoInfo 정보를 버퍼에서 뽑아옴 (byte array to string)
+            String newRivoInfo = new String(bufferToReceive, 7, bufferlen);
+
+            rivoInfo = newRivoInfo;             //원래의 설정값을 뽑아온 새 설정값으로 변경
+            System.out.println("newRivoInfo: " + newRivoInfo);
+            System.out.println("rivoInfo: " + rivoInfo);
+
+            bufferToSend[4] = (byte) (bufferlen & 0xFF);    //4, 5 index에 length 정보 세팅
+            bufferToSend[5] = (byte) ((bufferlen >> 8 )& 0xFF);
+
+            System.arraycopy(rivoInfo.getBytes(), 0, bufferToSend, 7, bufferlen);
+
+            DatagramPacket dpSend = new DatagramPacket(bufferToSend, bufferToSend.length, ia, 7000);
+            dsSend.send(dpSend);
+
+        }
+
+        //System.out.println(dp.getAddress().getHostAddress() + " > " + new String(dp.getData()).trim());
     }
 }

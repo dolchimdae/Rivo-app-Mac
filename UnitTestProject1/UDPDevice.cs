371a7a8fd0babe87433334342de9007e9229bace
KimJae0 <85392619+KimJae0@users.noreply.github.com>
2022-01-17 20:12:14 +0900

    Update UDPDevice.cs

--- UnitTestProject1/UDPDevice.cs
+++ UnitTestProject1/UDPDevice.cs
@@ -1,40 +1,90 @@
-﻿using System;
-using System.Threading.Tasks;
-using Windows.Networking;
-using Windows.Networking.Sockets;
-using Windows.Storage.Streams;
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using Windows.Networking;
+    using Windows.Networking.Sockets;
+    using Windows.Storage.Streams;
 
-namespace Rivo
-{
-    public class UDPDevice : RivoDevice
+    namespace Rivo
     {
-        String hostname = "127.0.0.1";
-        int port = 6999;
-        public UDPDevice(String hostname, int port)
+        public class UDPDevice : RivoDevice
         {
-            this.hostname = hostname;
-            this.port = port;
-        }
-        
-        public override async Task<byte[]> SendAndReceive(byte[] sendData)
-        {
-            var tcs = new TaskCompletionSource<byte[]>();
-            var socket = new DatagramSocket();
-            socket.MessageReceived += (sender, eventArgs) =>
+            String hostname = "127.0.0.1";
+            int port = 6999;
+            public UDPDevice(String hostname, int port)
             {
-                uint len = eventArgs.GetDataReader().UnconsumedBufferLength;
-                byte[] buffer = new byte[len];
-                eventArgs.GetDataReader().ReadBytes(buffer);
-                socket.Dispose();
-                tcs.TrySetResult(buffer);
-            };
-            
-            var ostream = await socket.GetOutputStreamAsync(new HostName(hostname), port.ToString());
-            var writer = new DataWriter(ostream);
-            writer.WriteBytes(sendData);
-            await writer.StoreAsync();
-            // XXX TODO create receive timer
-            return tcs.Task.Result;
+                this.hostname = hostname;
+                this.port = port;
+            }
+
+
+            public override async Task WritePacket(byte[] sendData)
+            {
+                /*
+                var tcs = new TaskCompletionSource<byte[]>();
+                var socket = new DatagramSocket();
+
+                var ostream = await socket.GetOutputStreamAsync(new HostName(hostname), port.ToString());
+                var writer = new DataWriter(ostream);
+                writer.WriteBytes(sendData);
+                await writer.StoreAsync();
+                // XXX TODO create receive timer
+                //return tcs.Task.Result;
+                */
+
+
+
+                var client = new UdpClient();
+                client.Connect("127.0.0.1", 6999);
+                await client.SendAsync(sendData, sendData.Length);
+            }
+
+            public override async Task<byte[]> readPacket()
+            {
+                /*
+                var tcs = new TaskCompletionSource<byte[]>();
+                var socket = new DatagramSocket();
+                socket.MessageReceived += (sender, eventArgs) =>
+                {
+                    uint len = eventArgs.GetDataReader().UnconsumedBufferLength;
+                    byte[] buffer = new byte[len];
+                    eventArgs.GetDataReader().ReadBytes(buffer);
+                    socket.Dispose();
+                    tcs.TrySetResult(buffer);
+                };
+                */
+
+                var client = new UdpClient();
+                var receiveDatagram = await client.ReceiveAsync();
+
+                byte[] buffer;
+                buffer = receiveDatagram.Buffer;
+
+                return buffer;
+            }
+
+            public override async Task<byte[]> ReadAndWrite(byte[] sendData)
+            {
+                {
+                    var tcs = new TaskCompletionSource<byte[]>();
+                    var socket = new DatagramSocket();
+                    socket.MessageReceived += (sender, eventArgs) =>
+                    {
+                        uint len = eventArgs.GetDataReader().UnconsumedBufferLength;
+                        byte[] buffer = new byte[len];
+                        eventArgs.GetDataReader().ReadBytes(buffer);
+                        socket.Dispose();
+                        tcs.TrySetResult(buffer);
+                    };
+
+                    var ostream = await socket.GetOutputStreamAsync(new HostName(hostname), port.ToString());
+                    var writer = new DataWriter(ostream);
+                    writer.WriteBytes(sendData);
+                    await writer.StoreAsync();
+                    // XXX TODO create receive timer
+                    return tcs.Task.Result;
+                }
+            }
         }
     }
-}
